33000ac7b06061ca912895e1b7b9f4c99cf4cba9
Mario Neises <git@mnio.net>
2016-03-27 14:56:34 +0200

    OperationPlayground Exercise

---
+++ src/OperationPlayground.java
@@ -0,0 +1,9 @@
+public class OperationPlayground {
+
+    public static void main(String[] args) {
+        int i = 5;
+        boolean isEven = i % 2 == 0;
+        String result = "Zahl " + i + " ist gerade: " + isEven;
+        System.out.println(result);
+    }
+}
--- src/folienExamples/Hello.java
+++ src/folienExamples/Hello.java
@@ -1,7 +1,9 @@
 package folienExamples;
 
 public class Hello {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
 }
